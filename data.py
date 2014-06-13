8ff436d974e63d7ccb1e246d63ddc82209b0f91a
Carlos Huamani <carlos.huamani@orbis.pe>
2014-06-13 18:02:51 -0500

    VALIDACIONES: refactoring del metodo

--- data.py
+++ data.py
@@ -1,39 +1,163 @@
 # -*- coding: utf-8 -*-
-C_MESSAGE_REQUIRED = 'Este campo es requerido.'
-C_MESSAGE_MAX = 'La longitud máxima es de %d caracteres.'
-C_MESSAGE_MIN = 'La longitud mínima es de %d caracteres.'
+C_MESSAGE_REQUIRED = u'Este campo es requerido.'
+C_MESSAGE_MAX = u'La longitud máxima es de %d caracteres.'
+C_MESSAGE_MIN = u'La longitud mínima es de %d caracteres.'
+C_MESSAGE_ONLY_NUMBERS = u'Ingrese solo números.'
 
-def makeDict(location, description):
-    return {'location': location, 'description': description}
+C_FIELD_NAME = 'txtName'
+C_FIELD_LASTNAME = 'txtLastName'
+C_FIELD_EMAIL = 'txtEmail'
+C_FIELD_PASSWORD = 'txtPassword1'
+C_FIELD_CONFIRM = 'txtRepeatPassword'
+C_FIELD_DOCUMENT = 'txtDocument'
+C_FIELD_PHONE_1 = 'txtPhone1'
+C_FIELD_PHONE_2 = 'txtPhone2'
+C_FIELD_ADDRESS = 'txtAddress'
+C_FIELD_CHECK_TERMS = 'chkTerms'
+
+CODIGO = dict()
+CODIGO[C_FIELD_NAME] = 'name'
+CODIGO[C_FIELD_LASTNAME] = 'last_name'
+CODIGO[C_FIELD_EMAIL] = 'email'
+CODIGO[C_FIELD_PASSWORD] = 'password'
+CODIGO[C_FIELD_CONFIRM] = 'confirm'
+CODIGO[C_FIELD_DOCUMENT] = 'document'
+CODIGO[C_FIELD_PHONE_1] = 'phone_1'
+CODIGO[C_FIELD_PHONE_2] = 'phone_2'
+CODIGO[C_FIELD_ADDRESS] = 'address'
+CODIGO[C_FIELD_CHECK_TERMS] = 'check_terms'
+
+C_VALUE_NAME = 'Carlos'
+C_VALUE_LASTNAME = 'Huamani'
+C_VALUE_EMAIL = 'carlos.hs.92@hola.com'
+C_VALUE_PASSWORD = 'carlos123'
+C_VALUE_CONFIRM = 'carlos123'
+C_VALUE_DOCUMENT_DNI = '78459598'
+C_VALUE_DOCUMENT_RUC = '78459598652'
+C_VALUE_DOCUMENT_PASSPORT = '5421235687'
+C_VALUE_DOCUMENT_CARD = '5421875421'
+C_VALUE_PHONE_1 = '7854859'
+C_VALUE_PHONE_2 = '87596598'
+C_VALUE_ADDRESS = 'Jose Galvez'
+C_VALUE_CHECK_TERMS = ''
+
+def makeDict(location, description, value):
+    return {'location': location, 'description': description, 'value': value, 'cod': CODIGO[location]}
+
+def makeRegister(cod, error=True, type_error=None, ubigeo=None, address=None, document=None):
+    register = {'prueba': cod,
+                'error': False,
+                'type_error': '',
+                'name': C_VALUE_NAME,
+                'last_name': C_VALUE_LASTNAME,
+                'email': C_VALUE_EMAIL,
+                'password': C_VALUE_PASSWORD,
+                'confirm': C_VALUE_CONFIRM,
+                'type_document': 'DNI',
+                'document': C_VALUE_DOCUMENT_DNI,
+                'phone_1': C_VALUE_PHONE_1,
+                'phone_2': C_VALUE_PHONE_2,
+                'type_address':'Calle',
+                'address': C_VALUE_ADDRESS,
+                'select_ubigeo': False,
+                'department':'',
+                'province':'',
+                'district':'',
+                'check_terms': True}
+    if address is not None:
+        register['type_address'] = address['type']
+        register['address'] = address['value']
+    if document is not None:
+        register['type_document'] = document['type']
+        register['document'] = document['value']
+    if type_error is not None:
+        register['error'] = error
+        register['type_error'] = type_error
+        register[type_error['cod']] = type_error['value']
+    if ubigeo is not None:
+        register['type_address'] = ubigeo['type']
+        register['department'] = ubigeo['department']
+        register['province'] = ubigeo['province']
+        register['district'] = ubigeo['district']
+
+    return register
 
 ERRORR_REGISTER = dict()
+ERRORR_REGISTER['c_name_validate'] = makeDict(C_FIELD_NAME, u'Solo letras, espacios y guiones (-)', 'Carlos123')
+ERRORR_REGISTER['c_name_min'] = makeDict(C_FIELD_NAME, C_MESSAGE_MIN % 2, 'C')
+ERRORR_REGISTER['c_name_max'] = makeDict(C_FIELD_NAME, C_MESSAGE_MAX % 50, 'Carlos'*9)
+ERRORR_REGISTER['c_name_required'] = makeDict(C_FIELD_NAME, C_MESSAGE_REQUIRED, '')
 
-ERRORR_REGISTER['c_name_letter'] = makeDict('name', 'Solo letras, espacios y guiones (-)')
-ERRORR_REGISTER['c_name_min'] = makeDict('name', C_MESSAGE_MIN % 2)
-ERRORR_REGISTER['c_name_max'] = makeDict('name', C_MESSAGE_MAX % 50)
-ERRORR_REGISTER['c_name_required'] = makeDict('name', C_MESSAGE_REQUIRED)
+ERRORR_REGISTER['c_last_name_validate'] = makeDict(C_FIELD_LASTNAME, u'Solo letras, espacios y guiones (-)', 'Huamani$')
+ERRORR_REGISTER['c_last_name_min'] = makeDict(C_FIELD_LASTNAME, C_MESSAGE_MIN % 2, 'H')
+ERRORR_REGISTER['c_last_name_max'] = makeDict(C_FIELD_LASTNAME, C_MESSAGE_MAX % 50, 'Huamani'*10)
+ERRORR_REGISTER['c_last_name_required'] = makeDict(C_FIELD_LASTNAME, C_MESSAGE_REQUIRED, '')
 
-ERRORR_REGISTER['c_lastname_letter'] = makeDict('location', 'Solo letras, espacios y guiones (-)')
-ERRORR_REGISTER['c_lastname_min'] = makeDict('location', C_MESSAGE_MIN % 2)
-ERRORR_REGISTER['c_lastname_max'] = makeDict('location', C_MESSAGE_MAX % 50)
-ERRORR_REGISTER['c_name_required'] = makeDict('location', C_MESSAGE_REQUIRED)
+ERRORR_REGISTER['c_email_validate'] = makeDict(C_FIELD_EMAIL, u'Ingrese un email válido.', 'demo.pe')
+ERRORR_REGISTER['c_email_max'] = makeDict(C_FIELD_EMAIL, C_MESSAGE_MAX % 50, '%s@hotmail.com'%('carlos'*10))
+ERRORR_REGISTER['c_email_required'] = makeDict(C_FIELD_EMAIL, C_MESSAGE_REQUIRED, '')
+ERRORR_REGISTER['c_email_unique'] = makeDict(C_FIELD_EMAIL, u'El email está siendo actualmente usado', 'carlos.hs.92@gmail.com')
 
-ERRORR_REGISTER['c_email_validate'] = makeDict('email', 'Ingrese un email válido.')
-ERRORR_REGISTER['c_email_max'] = makeDict('email', C_MESSAGE_MAX % 50)
-ERRORR_REGISTER['c_email_required'] = makeDict('email', C_MESSAGE_REQUIRED)
+ERRORR_REGISTER['c_password_min'] = makeDict(C_FIELD_PASSWORD, C_MESSAGE_MIN % 6, 'rwrw')
+ERRORR_REGISTER['c_password_max'] = makeDict(C_FIELD_PASSWORD, C_MESSAGE_MAX % 20, 'rw'*20)
+ERRORR_REGISTER['c_password_required'] = makeDict(C_FIELD_PASSWORD, C_MESSAGE_REQUIRED, '')
 
-ERRORR_REGISTER['c_password_min'] = makeDict('password', C_MESSAGE_MIN % 6)
-ERRORR_REGISTER['c_password_max'] = makeDict('password', C_MESSAGE_MAX % 20)
-ERRORR_REGISTER['c_password_required'] = makeDict('password', C_MESSAGE_REQUIRED)
+ERRORR_REGISTER['c_confirm_validate'] = makeDict(C_FIELD_CONFIRM, u'No coincide con el campo contraseña', 'rwrwrwrw')
+ERRORR_REGISTER['c_confirm_required'] = makeDict(C_FIELD_CONFIRM, C_MESSAGE_REQUIRED, '')
+#ERRORR_REGISTER['c_confirm_max'] = makeDict('confirm', C_MESSAGE_MAX % 20) NECESITA UN MAX_LENGTH
 
-ERRORR_REGISTER['c_confirm_validate'] = makeDict('confirm', 'No coincide con el campo contraseña')
-ERRORR_REGISTER['c_confirm_required'] = makeDict('confirm', C_MESSAGE_REQUIRED)
+ERRORR_REGISTER['c_document_dni_max'] = makeDict(C_FIELD_DOCUMENT, u'El DNI debe ser de 8 dígitos', '2562541152')
+ERRORR_REGISTER['c_document_dni_min'] = makeDict(C_FIELD_DOCUMENT, u'El DNI debe ser de 8 dígitos', '25415')
+ERRORR_REGISTER['c_document_dni_validate'] = makeDict(C_FIELD_DOCUMENT, u'Ingrese un DNI válido', '!"#$%&/(')
+ERRORR_REGISTER['c_document_ruc_max'] = makeDict(C_FIELD_DOCUMENT, u'El RUC debe ser de 11 dígitos', '25415236598208')
+ERRORR_REGISTER['c_document_ruc_min'] = makeDict(C_FIELD_DOCUMENT, u'El RUC debe ser de 11 dígitos', '25415')
+ERRORR_REGISTER['c_document_ruc_validate'] = makeDict(C_FIELD_DOCUMENT, u'Ingrese un RUC válido', 'ASWQWERTYHU')
+ERRORR_REGISTER['c_document_passport_max'] = makeDict(C_FIELD_DOCUMENT, u'Ingrese un máximo de 12 caracteres', '321456987456587')
+#ERRORR_REGISTER['c_document_passport_min'] = makeDict(C_FIELD_DOCUMENT, 'Ingrese un pasaporte válido', '254')
+ERRORR_REGISTER['c_document_passport_validate'] = makeDict(C_FIELD_DOCUMENT, u'Ingrese un pasaporte válido', 'QWETERS')
+ERRORR_REGISTER['c_document_card_max'] = makeDict(C_FIELD_DOCUMENT, u'Ingrese un máximo de 12 caracteres', '55789545354426')
+#ERRORR_REGISTER['c_document_card_min'] = makeDict(C_FIELD_DOCUMENT, 'Ingrese un carnet de extranjería válido')
+ERRORR_REGISTER['c_document_card_validate'] = makeDict(C_FIELD_DOCUMENT, u'Ingrese un carnet de extranjería válido', 'TERTSD')
+ERRORR_REGISTER['c_document_required'] = makeDict(C_FIELD_DOCUMENT, C_MESSAGE_REQUIRED, '')
 
+ERRORR_REGISTER['c_phone_1_min'] = makeDict(C_FIELD_PHONE_1, C_MESSAGE_MIN % 7, '54632')
+ERRORR_REGISTER['c_phone_1_max'] = makeDict(C_FIELD_PHONE_1, C_MESSAGE_MAX % 10, '2312987541545')
+ERRORR_REGISTER['c_phone_1_only_numbers'] = makeDict(C_FIELD_PHONE_1, C_MESSAGE_ONLY_NUMBERS, 'ASDQWEQWE')
+ERRORR_REGISTER['c_phone_required'] = makeDict(C_FIELD_PHONE_1, C_MESSAGE_REQUIRED, '')
+
+ERRORR_REGISTER['c_phone_2_min'] = makeDict(C_FIELD_PHONE_2, u'Ingrese un mínimo de 3 caracteres', '12')
+ERRORR_REGISTER['c_phone_2_max'] = makeDict(C_FIELD_PHONE_2, u'Ingrese un máximo de 10 caracteres', '25445687584152')
+ERRORR_REGISTER['c_phone_2_validate'] = makeDict(C_FIELD_PHONE_2, u'Sólo números, asterisco(*), guion(-) y numeral(#).', '!"#$%&/')
+
+ERRORR_REGISTER['c_address_validate'] = makeDict(C_FIELD_ADDRESS, u"Solo letras, espacios, guiones (-) y apóstrofes (')", '&%$#$#$')
+ERRORR_REGISTER['c_address_max'] = makeDict(C_FIELD_ADDRESS, C_MESSAGE_MAX % 150, 'JAJAJAJOJO'*20)
+
+ERRORR_REGISTER['c_terms_required'] = makeDict(C_FIELD_CHECK_TERMS, C_MESSAGE_REQUIRED, '')
+
+##-------------------------------------------------------------------------------------------------------------------
 REGISTER = list()
-REGISTER.append({'error': False,
-             'name': 'Carlos', 'last_name': 'Huamani', 'email':'carlos.hs.92@gmail.com',
-             'password': 'carlos123', 'password_confirm':'carlos_123',
-             'type_document':'DNI','document':'25413698',
-             'phone1':'4569877', 'phone2':'45263299',
-             'type_direction':'Calle','direccion':'Jose Galvez 123',
-             'ubigeo': False})
+REGISTER.append(makeRegister('registro_1'))
+
+REGISTER.append(makeRegister('registro_2', type_error=ERRORR_REGISTER['c_name_validate']))
+REGISTER.append(makeRegister('registro_3', type_error=ERRORR_REGISTER['c_name_min']))
+REGISTER.append(makeRegister('registro_4', type_error=ERRORR_REGISTER['c_name_required']))
+REGISTER.append(makeRegister('registro_5', error=False, type_error=ERRORR_REGISTER['c_last_name_max']))
+
+REGISTER.append(makeRegister('registro_6', type_error=ERRORR_REGISTER['c_last_name_validate']))
+REGISTER.append(makeRegister('registro_7', type_error=ERRORR_REGISTER['c_last_name_min']))
+REGISTER.append(makeRegister('registro_8', type_error=ERRORR_REGISTER['c_last_name_required']))
+REGISTER.append(makeRegister('registro_9', error=False, type_error=ERRORR_REGISTER['c_last_name_max']))
+
+REGISTER.append(makeRegister('registro_10', type_error=ERRORR_REGISTER['c_email_required']))
+REGISTER.append(makeRegister('registro_11', type_error=ERRORR_REGISTER['c_email_unique']))
+REGISTER.append(makeRegister('registro_12', type_error=ERRORR_REGISTER['c_email_validate']))
+REGISTER.append(makeRegister('registro_13', error=False, type_error=ERRORR_REGISTER['c_email_max']))
+
+REGISTER.append(makeRegister('registro_14', type_error=ERRORR_REGISTER['c_password_min']))
+REGISTER.append(makeRegister('registro_15', type_error=ERRORR_REGISTER['c_password_required']))
+REGISTER.append(makeRegister('registro_16', error=False, type_error=ERRORR_REGISTER['c_password_max']))
+
+REGISTER.append(makeRegister('registro_17', type_error=ERRORR_REGISTER['c_confirm_required']))
+REGISTER.append(makeRegister('registro_18', type_error=ERRORR_REGISTER['c_confirm_validate']))
+
+REGISTER.append(makeRegister('registro_19', type_error=ERRORR_REGISTER['c_document_dni_validate']))
--- pruebas_de_registro.py
+++ pruebas_de_registro.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from data import REGISTER
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,11 +12,16 @@ class PruebasDeRegistro(unittest.TestCase):
     def setUp(self):
         self.binary = FirefoxBinary('/usr/bin/firefox/firefox')
         self.driver = webdriver.Firefox(firefox_binary=self.binary)
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(90)
         self.base_url = "http://local.ofertop.pe/"
         self.verificationErrors = []
-        self.accept_next_alert = True
-    
+        self.accept_next_alert = False
+        self.debug = False
+
+    def printText(self, text):
+        if self.debug:
+            print text
+
     def test_pruebas_de_registro(self):
         driver = self.driver
         driver.get(self.base_url + "/")
@@ -23,30 +29,75 @@ class PruebasDeRegistro(unittest.TestCase):
         driver.find_element_by_css_selector(u"li.register_link > a[title=\"Regístrate\"]").click()
         driver.find_element_by_css_selector("a.fancybox-item.fancybox-close").click()
 
-        driver.find_element_by_id("txtName").clear()
-        driver.find_element_by_id("txtName").send_keys("prueba")
-        driver.find_element_by_id("txtLastName").clear()
-        driver.find_element_by_id("txtLastName").send_keys("demo")
-        driver.find_element_by_css_selector("div.form_control.relative > #txtEmail").clear()
-        driver.find_element_by_css_selector("div.form_control.relative > #txtEmail").send_keys("carlos.hs.92@demo.com")
-        driver.find_element_by_id("txtPassword1").clear()
-        driver.find_element_by_id("txtPassword1").send_keys("carlos123")
-        driver.find_element_by_id("txtRepeatPassword").clear()
-        driver.find_element_by_id("txtRepeatPassword").send_keys("carlos123")
-        driver.find_element_by_id("txtDocument").clear()
-        driver.find_element_by_id("txtDocument").send_keys("46941256")
-        driver.find_element_by_id("txtPhone1").clear()
-        driver.find_element_by_id("txtPhone1").send_keys("25415231")
-        driver.find_element_by_id("txtPhone2").clear()
-        driver.find_element_by_id("txtPhone2").send_keys("1155995588")
-        Select(driver.find_element_by_id("selAddress")).select_by_visible_text("Calle")
-        driver.find_element_by_id("txtAddress").clear()
-        driver.find_element_by_id("txtAddress").send_keys("Santa Anita")
-        Select(driver.find_element_by_id("selProvince")).select_by_visible_text("Oyon")
-        Select(driver.find_element_by_id("selDistrict")).select_by_visible_text("Caujul")
-        driver.find_element_by_id("chkTerms").click()
+        for data in REGISTER:
+            print data['prueba']
+            driver.find_element_by_id("txtName").clear()
+            driver.find_element_by_id("txtName").send_keys(data['name'])
+            self.printText( 'nombre ingresado')
 
-        driver.find_element_by_id("sbmRegister").click()
+            driver.find_element_by_id("txtLastName").clear()
+            driver.find_element_by_id("txtLastName").send_keys(data['last_name'])
+            self.printText('apellido ingresado')
+
+            driver.find_element_by_css_selector("div.form_control.relative > #txtEmail").clear()
+            driver.find_element_by_css_selector("div.form_control.relative > #txtEmail").send_keys(data['email'])
+            self.printText( 'correo ingresado' )
+
+            driver.find_element_by_id("txtPassword1").clear()
+            driver.find_element_by_id("txtPassword1").send_keys(data['password'])
+            self.printText( 'contraseña ingresado')
+
+            driver.find_element_by_id("txtRepeatPassword").clear()
+            driver.find_element_by_id("txtRepeatPassword").send_keys(data['confirm'])
+            self.printText( 'contraseña repetida ingresado')
+
+            Select(driver.find_element_by_id("selDocument")).select_by_visible_text(data['type_document'])
+            driver.find_element_by_id("txtDocument").clear()
+            driver.find_element_by_id("txtDocument").send_keys(data['document'])
+            self.printText( 'documento ingresado')
+
+            driver.find_element_by_id("txtPhone1").clear()
+            driver.find_element_by_id("txtPhone1").send_keys(data['phone_1'])
+            self.printText( 'telefono 1 ingresado')
+
+            driver.find_element_by_id("txtPhone2").clear()
+            driver.find_element_by_id("txtPhone2").send_keys(data['phone_2'])
+            self.printText( 'telefono 2 ingresado')
+
+            Select(driver.find_element_by_id("selAddress")).select_by_visible_text(data['type_address'])
+            driver.find_element_by_id("txtAddress").clear()
+            driver.find_element_by_id("txtAddress").send_keys(data['address'])
+            self.printText( 'direccion ingresado')
+
+
+            if data['select_ubigeo']:
+                Select(driver.find_element_by_id("selProvince")).select_by_visible_text("Oyon")
+                Select(driver.find_element_by_id("selDistrict")).select_by_visible_text("Caujul")
+                self.printText( 'ubigeo ingresado')
+
+            if data['check_terms']:
+                driver.find_element_by_id("chkTerms").click()
+                self.printText( 'checks ingresado')
+
+            #driver.find_element_by_id("sbmRegister").click()
+
+            if data['check_terms']:
+                driver.find_element_by_id("chkTerms").click()
+                self.printText( 'checks ingresado')
+
+            if data['error']:
+                if data['type_error']['location'] != 'txtEmail':
+                    error_id = driver.find_element_by_id(data['type_error']['location']).get_attribute('data-parsley-id')
+                else:
+                    error_id = driver.find_element_by_css_selector("div.form_control.relative > #txtEmail").get_attribute('data-parsley-id')
+                print error_id
+                message = driver.find_element_by_css_selector('#parsley-id-%s > li' % error_id).text
+                assert data['type_error']['description'] == message, u'Mensaje erroneo en la prueba: %s , %s == %s' % (data['prueba'], data['type_error']['description'], message)
+                print u'Exito en %s' % data['type_error']['description']
+
+
+            #driver.find_element_by_css_selector(u"li.register_link > a[title=\"Regístrate\"]").click()
+            #driver.find_element_by_css_selector("a.fancybox-item.fancybox-close").click()
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
